e9cf8978f7057e9cdcf5b5cf90416c75ecca82ad
lauren_m <laurenamolloy@gmail.com>
2025-04-01 14:26:18 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+console.log("it works")
+const toggle = document.querySelector(".toggleBtn");
+const closeBtn = document.querySelector(".close-btn");
+const sideBar = document.querySelector(".side");
+
+//Toggle show class on side nav
+toggle.addEventListener("click", function() {
+    console.log(sideBar.classList)
+    //The long way
+    // if(sideBar.classList.contains('show-sidebar')) {
+    //     sideBar.classList.remove('show-sideBar');
+    // } else {
+    //     sideBar.classList.add("show-sidebar");
+    // }
+    sideBar.classList.toggle('show-sidebar')
+})
+
+closeBtn.addEventListener("click", function() {
+    console.log("clicks")
+    sideBar.classList.remove('show-sidebar');
+})
