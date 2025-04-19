029a4513f020864b4cc0e5c3188e7f09d70f9a4c
lauren_m <laurenamolloy@gmail.com>
2025-04-19 15:03:15 +0300

    Added language toggle

--- script.js
+++ script.js
@@ -1,11 +1,7 @@
-console.log("it works")
 const toggle = document.querySelector(".toggleBtn");
 const closeBtn = document.querySelector(".close-btn");
 const sideBar = document.querySelector(".side");
 const welcome= document.getElementById("welcome")
-
-
-
 
 //Toggle show class on side nav
 toggle.addEventListener("click", function() {
@@ -23,3 +19,55 @@ closeBtn.addEventListener("click", function() {
     console.log("clicks")
     sideBar.classList.remove('show-sidebar');
 })
+
+//Function to fetch langauge data
+async function fetchLanguageData(lang) {
+    try {
+        const response = await fetch(`languages/${lang}.json`)
+        if(!response.ok) throw new Error("Langauge")
+        return response.json()
+    
+    } catch (err) {
+        console.error("Error loading langauge:", err)
+        // fallback empty object
+        return {};
+    }
+    
+}
+
+//Function to set language preference
+function setLanguagePreference(lang) {
+    localStorage.setItem('language', lang)
+    //location.reload();
+}
+
+//Function to update content based on selected langauge
+//Iterate over elements with data attributes
+//Update text content using corresponding translations
+function updateContent(langData) {
+    //grab all elements with data-i18n
+    document.querySelectorAll('[data-i18n]').forEach(element => {
+        //retrive the data attribute
+        const key = element.getAttribute('data-i18n');
+        //look up the translated text in the lang data object
+        element.innerHTML = langData[key] || key
+    });
+}
+
+//Function to change language
+async function changeLanguage(lang) {
+    //Set lang to local storage
+    //Lang persists even after they leave the website
+    await setLanguagePreference(lang);
+    
+    //Retrieve content from json files
+    const langData = await fetchLanguageData(lang)
+    updateContent(langData);
+}
+
+// Call updateContent() on page load
+window.addEventListener("DOMContentLoaded", async () => {
+    const userPreferredLanguage = localStorage.getItem("language") || "en";
+    const langData = await fetchLanguageData(userPreferredLanguage);
+    updateContent(langData);
+  });
